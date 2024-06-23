1ef480049e0f579c51eda86a3a2b106fd74172a5
Maciej Cynkar <s27003@pjwstk.edu.pl>
2024-06-23 22:03:35 +0200

    Done

--- APBD-4/APBD-4/Visit/VisitService.cs
+++ APBD-4/APBD-4/Visit/VisitService.cs
@@ -2,5 +2,26 @@
 
 public class VisitService : IVisitService
 {
+    private List<Visit> _visits = new List<Visit>();
+
+    public VisitService()
+    {
+        _visits.Add(new Visit(new DateTime(2022, 1, 1), 1, "Visit 1", 1009));
+        _visits.Add(new Visit(new DateTime(2021, 1, 2), 1, "Visit 2", 435));
+        _visits.Add(new Visit(new DateTime(2021, 1, 3), 2, "Visit 3", 1523));
+        _visits.Add(new Visit(new DateTime(2021, 1, 2), 1, "Visit 4", 342));
+        _visits.Add(new Visit(new DateTime(2021, 1, 3), 2, "Visit 5", 133));
+    }
     
+
+    public IEnumerable<Visit> GetVisitsByAnimalId(int animalId)
+    {
+        return _visits.FindAll(x => x.AnimalId == animalId);
+    }
+    
+    public Visit AddVisit(Visit visit)
+    {
+        _visits.Add(visit);
+        return visit;
+    }
 }
--- APBD-4/APBD-4/obj/Debug/net8.0/APBD-4.AssemblyInfo.cs
+++ APBD-4/APBD-4/obj/Debug/net8.0/APBD-4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD-4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66dc42cca4acc34377429fd0078907073f032a0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD-4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD-4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
